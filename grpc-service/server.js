76fec25827ab9ceccdbd66ce5acf23aba4ff4acd
ades012 <gngsptn@gmail.com>
2025-07-03 11:02:00 +0700

    add container 2

---
+++ grpc-service/server.js
@@ -0,0 +1,28 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const sqlite3 = require('better-sqlite3');
+const path = require('path');
+
+const PROTO_PATH = path.join(__dirname, 'protos', 'inventory.proto');
+const db = new sqlite3('/data/inventory.db');
+
+const packageDef = protoLoader.loadSync(PROTO_PATH);
+const grpcObj = grpc.loadPackageDefinition(packageDef);
+const inventory = grpcObj.inventory;
+
+function GetItemById(call, callback) {
+  const id = call.request.id;
+  const row = db.prepare('SELECT * FROM items WHERE id = ?').get(id);
+  if (row) {
+    callback(null, { id: row.id, name: row.name });
+  } else {
+    callback(null, { id: 0, name: 'Not Found' });
+  }
+}
+
+const server = new grpc.Server();
+server.addService(inventory.InventoryService.service, { GetItemById });
+server.bindAsync('0.0.0.0:5000', grpc.ServerCredentials.createInsecure(), () => {
+  console.log('gRPC server running at port 5000');
+  server.start();
+});
